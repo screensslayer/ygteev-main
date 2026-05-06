643f06d9d25991273f62b6ec5d7b47c22df7c4be
screensslayer <jim@storybutton.com>
2026-05-06 12:29:17 -0400

    Initial Commit

---
+++ YGTeeV/YGTeeVApp.swift
@@ -0,0 +1,17 @@
+//
+//  YGTeeVApp.swift
+//  YGTeeV
+//
+//  Created by Jim Jacob on 5/6/26.
+//
+
+import SwiftUI
+
+@main
+struct YGTeeVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
